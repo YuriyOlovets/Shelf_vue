63c03d1e8a6fa973d88a557cd1e5159a74af7e68
Yuriy Olovets <74701895+YuriyOlovets@users.noreply.github.com>
2021-09-24 18:22:10 +0300

    ref

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -48,7 +48,7 @@ export default {
     async setRegister() {
       await axios({
         method: 'post',
-        url: 'http://127.0.0.1:8000/auth/users/',
+        url: 'http://smart-shelf.net/auth/users/',
         data:{
           username: this.login,
           password: this.password
